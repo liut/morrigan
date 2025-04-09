50a883998126b471084b8dbcc6206ddee1ab40e4
liut <liutao@liut.cc>
2025-04-09 23:25:59 +0800

    fix: CI build; delete unused file

--- data/schemas/20230415173800_qa_doc.up.sql
+++
@@ -1,8 +0,0 @@
-
-
-ALTER TABLE IF EXISTS qa_corpus_document DROP IF EXISTS tokens;
-ALTER TABLE IF EXISTS qa_corpus_document DROP IF EXISTS embedding;
-
-CREATE INDEX ON qa_corpus_prompt
-USING ivfflat (embedding vector_cosine_ops)
-WITH (lists = 100);
